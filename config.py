31bed4847f8dd25e21984cf5f30f629a0447c6df
Rakesh163-213 <naampata35217793@gmail.com>
2025-06-15 20:59:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # Copyright (C) @TheSmartBisnu
 # Channel: https://t.me/itsSmartDev
-
+import os
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXXXXXXX"  # Replace this BOT_TOKEN
-SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXX" # Replace this SESSION STRING with your actual SESSION_STRING
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2GB in bytes
